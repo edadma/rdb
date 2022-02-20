dce33fb622094ba1d1414c6c50e5afe580f2a88f
Edward A Maxedon Sr <edadma@gmail.com>
2022-02-20 15:15:16 -0500

    recode scalar function support

--- src/main/scala/io/github/edadma/rdb/Expr.scala
+++ src/main/scala/io/github/edadma/rdb/Expr.scala
@@ -7,9 +7,9 @@ case class Ident(s: String, pos: Option[Pos] = None)
 trait Expr:
   val typ: Type
 
-case class VariableExpr(name: Ident, typ: Type) extends Expr
-case class UnaryExpr(op: String, expr: Expr, typ: Type) extends Expr
-case class BinaryExpr(left: Expr, op: String, right: Expr, typ: Type) extends Expr
+case class VariableExpr(name: Ident, typ: Type = UnknownType) extends Expr
+case class UnaryExpr(op: String, expr: Expr, typ: Type = UnknownType) extends Expr
+case class BinaryExpr(left: Expr, op: String, right: Expr, typ: Type = UnknownType) extends Expr
 case class StringExpr(s: String, pos: Option[Pos] = None) extends Expr { val typ: Type = StringType }
 case class NumberExpr(n: Number, pos: Option[Pos] = None) extends Expr { val typ: Type = NumberType }
 
@@ -21,6 +21,7 @@ case class TableStarExpr(table: Ident) extends UnknownTypeExpr
 case class ApplyExpr(name: Ident, args: Seq[Expr]) extends UnknownTypeExpr
 
 case class ScalarFunctionExpr(f: ScalarFunction, typ: Type) extends Expr
+case class AggregateFunctionExpr(f: ScalarFunction, typ: Type) extends Expr
 
 trait TableTypeExpr extends Expr { val typ: Type = TableType }
 
@@ -28,7 +29,7 @@ case class AliasExpr(expr: Expr, alias: Ident) extends TableTypeExpr
 
 // SQL
 
-case class SQLSelectExpr(exprs: Seq[Expr], from: Seq[TableExpr], where: Option[Expr]) extends TableTypeExpr
+case class SQLSelectExpr(exprs: Seq[Expr], from: Seq[Expr], where: Option[Expr]) extends TableTypeExpr
 
 // relational
 
--- src/main/scala/io/github/edadma/rdb/rewrite.scala
+++ src/main/scala/io/github/edadma/rdb/rewrite.scala
@@ -2,7 +2,17 @@ package io.github.edadma.rdb
 
 def rewrite(expr: Expr): Expr =
   expr match
-    case ApplyExpr(name, args)          =>
+    case ApplyExpr(name, args) =>
+      scalarFunction get name match
+        case None    => sys.error(s"unknown function '$name'")
+        case Some(f) => ScalarFunctionExpr(f, f.typ)
     case BinaryExpr(left, op, right, _) =>
-    case SQLSelectExpr(oper, _)         =>
-    case _                              => expr
+      val l = rewrite(left)
+      val r = rewrite(right)
+
+      if (l.typ != r.typ) sys.error(s"type mismatch: ${l.typ}, ${r.typ}")
+
+      BinaryExpr(l, op, r, l.typ)
+    case SQLSelectExpr(exprs, from, where) =>
+
+    case _ => expr
