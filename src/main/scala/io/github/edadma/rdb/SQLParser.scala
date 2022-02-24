260cbabc08396d60e416d094ffd2a3a0d3858d22
Edward A Maxedon Sr <edadma@gmail.com>
2022-02-23 20:16:30 -0500

    cont coding SQL parser

--- src/main/scala/io/github/edadma/rdb/SQLParser.scala
+++ src/main/scala/io/github/edadma/rdb/SQLParser.scala
@@ -149,3 +149,20 @@ object SQLParser extends RegexParsers with PackratParsers {
     }
 
 }
+
+def problem(pos: Position, msg: String, input: String): Nothing = {
+  printError(pos, msg, input)
+  sys.error("error executing query")
+}
+
+def printError(pos: Position, msg: String, input: String): Null = {
+  if (pos eq null)
+    Console.err.println(msg)
+  else if (pos.line == 1)
+    Console.err.println(s"$msg\n${pos.longString}")
+  else
+    Console.err.println(s"${pos.line}: $msg\n${pos.longString}")
+  //      printError(pos.line, pos.col, msg, input)
+
+  null
+}
