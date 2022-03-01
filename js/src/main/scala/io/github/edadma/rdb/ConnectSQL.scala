726915e120732bd6520326ce5a57432403e501c3
Edward A Maxedon Sr <edadma@gmail.com>
2022-03-01 15:41:23 -0500

    convert rdb values to javascript

--- js/src/main/scala/io/github/edadma/rdb/ConnectSQL.scala
+++ js/src/main/scala/io/github/edadma/rdb/ConnectSQL.scala
@@ -1,5 +1,7 @@
 package io.github.edadma.rdb
 
+import io.github.edadma.dal.{IntType => DIntType, DoubleType => DDoubleType}
+
 import scala.scalajs.js
 import js.JSConverters._
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
@@ -9,16 +11,26 @@ class ConnectSQL():
 
   val db = new MemoryDB()
 
+  private def toJS(v: Value): js.Any =
+    v match
+      case NumberValue(DIntType, n)    => n.intValue
+      case NumberValue(DDoubleType, n) => n.doubleValue
+      case TextValue(s)                => s
+      case NullValue()                 => null
+      case ArrayValue(elems)           => elems map toJS toJSArray
+      case ObjectValue(properties)     => (properties map { case (k, v) => k -> toJS(v) } toMap) toJSDictionary
+      case TimestampValue(t)           => new js.Date(t.toString)
+
   @JSExport
   def execute(sql: String): Any =
     executeSQL(sql)(db) match
       case CreateTableResult(table) =>
         js.Dynamic.literal(command = "create table", table = table)
       case InsertResult(result) =>
-        val res = result.view.mapValues(_.toText.s).toMap.toJSDictionary
+        val res = result.view.mapValues(toJS).toMap.toJSDictionary
 
         js.Dynamic.literal(command = "insert", result = res)
       case QueryResult(table) =>
-        val res = table.data map (_.data map (_.toText.s) toJSArray) toJSArray
+        val res = table.data map (_.data map toJS toJSArray) toJSArray
 
         js.Dynamic.literal(command = "select", result = res)
