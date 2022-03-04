4cc2d34a0b608529de45357954502ae79bdb31df
Edward A Maxedon Sr <edadma@gmail.com>
2022-03-04 12:23:19 -0500

    FIX parser

--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@ lazy val rdb = crossProject(JSPlatform, JVMPlatform /*, NativePlatform*/ )
   .in(file("."))
   .settings(
     name := "rdb",
-    version := "0.1.0-pre.17",
+    version := "0.1.0-pre.18",
     scalaVersion := "3.1.1",
     scalacOptions ++=
       Seq(
--- shared/src/main/scala/io/github/edadma/rdb/SQLParser.scala
+++ shared/src/main/scala/io/github/edadma/rdb/SQLParser.scala
@@ -174,7 +174,7 @@ object SQLParser extends StandardTokenParsers with PackratParsers:
   lazy val pos: P[Position] = positioned(success(new Positional {})) ^^ (_.pos)
 
   lazy val query: P[SQLSelectExpr] =
-    "SELECT" ~ expressions ~ fromClause ~ whereClause ~ groupByClause ~ orderByClause ~ offsetClause ~ limitClause ^^ {
+    "SELECT" ~ expressions ~ fromClause ~ whereClause ~ groupByClause ~ orderByClause ~ limitClause ~ offsetClause ^^ {
       case _ ~ p ~ f ~ w ~ g ~ o ~ of ~ l =>
         SQLSelectExpr(p to ArraySeq, f, w, g, o, of, l)
     }
