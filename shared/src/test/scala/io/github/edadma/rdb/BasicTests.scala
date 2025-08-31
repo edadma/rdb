b378791db55c2839e0d740b19735a42365efaecb
Edward A Maxedon <edadma@gmail.com>
2025-08-31 18:23:51 -0400

    fix BasicTests expected output format
    Update test expectations to match actual output format
    after PostgreSQL modernization

--- shared/src/test/scala/io/github/edadma/rdb/BasicTests.scala
+++ shared/src/test/scala/io/github/edadma/rdb/BasicTests.scala
@@ -20,87 +20,75 @@ class BasicTests extends AnyFreeSpec with Matchers with Testing {
     ) shouldBe
       """
         |List(
-        |  CreateTableResult(table = "t"),
+        |  CreateTableResult("t"),
         |  InsertResult(
-        |    obj = Map("c1" -> NumberValue(typ = SerialType, value = 1)),
+        |    obj = Map("c1" -> NumberValue(typ = IntType, value = 1)),
         |    table = TableValue(
         |      data = Vector(
         |        Row(
         |          data = Vector(NumberValue(typ = IntType, value = 1)),
-        |          meta = Metadata(
-        |            columns = Vector(
-        |              ColumnMetadata(table = Some(value = "t"), name = "c1", typ = NumberType)
-        |            )
-        |          ),
+        |          meta = Metadata(Vector(ColumnMetadata(table = Some("t"), name = "c1", typ = NumberType))),
         |          updater = None,
         |          deleter = None,
         |          mode = Return
         |        )
         |      ),
-        |      meta = Metadata(
-        |        columns = Vector(ColumnMetadata(table = Some(value = "t"), name = "c1", typ = SerialType))
-        |      )
+        |      meta = Metadata(Vector(ColumnMetadata(table = Some("t"), name = "c1", typ = NumberType)))
         |    )
         |  ),
         |  InsertResult(
-        |    obj = Map("c1" -> NumberValue(typ = SerialType, value = 2)),
+        |    obj = Map("c1" -> NumberValue(typ = IntType, value = 2)),
         |    table = TableValue(
         |      data = Vector(
         |        Row(
         |          data = Vector(NumberValue(typ = IntType, value = 2)),
-        |          meta = Metadata(
-        |            columns = Vector(
-        |              ColumnMetadata(table = Some(value = "t"), name = "c1", typ = SerialType)
-        |            )
-        |          ),
+        |          meta = Metadata(Vector(ColumnMetadata(table = Some("t"), name = "c1", typ = NumberType))),
         |          updater = None,
         |          deleter = None,
         |          mode = Return
         |        )
         |      ),
-        |      meta = Metadata(
-        |        columns = Vector(ColumnMetadata(table = Some(value = "t"), name = "c1", typ = NumberType))
-        |      )
+        |      meta = Metadata(Vector(ColumnMetadata(table = Some("t"), name = "c1", typ = NumberType)))
         |    )
         |  ),
         |  QueryResult(
-        |    table = TableValue(
+        |    TableValue(
         |      data = ArraySeq(
         |        Row(
         |          data = ArraySeq(
-        |            NumberValue(typ = SerialType, value = 1),
+        |            NumberValue(typ = IntType, value = 1),
         |            NumberValue(typ = IntType, value = 123)
         |          ),
         |          meta = Metadata(
-        |            columns = ArraySeq(
-        |              ColumnMetadata(table = Some(value = "t"), name = "c1", typ = SerialType),
-        |              ColumnMetadata(table = Some(value = "t"), name = "c2", typ = IntegerType)
+        |            ArraySeq(
+        |              ColumnMetadata(table = Some("t"), name = "c1", typ = SerialType),
+        |              ColumnMetadata(table = Some("t"), name = "c2", typ = IntegerType)
         |            )
         |          ),
-        |          updater = Some(value = [MemoryDB Updater]),
-        |          deleter = Some(value = [MemoryDB Deleter]),
+        |          updater = Some([MemoryDB Updater]),
+        |          deleter = Some([MemoryDB Deleter]),
         |          mode = Return
         |        ),
         |        Row(
         |          data = ArraySeq(
-        |            NumberValue(typ = SerialType, value = 2),
+        |            NumberValue(typ = IntType, value = 2),
         |            NumberValue(typ = IntType, value = 456)
         |          ),
         |          meta = Metadata(
-        |            columns = ArraySeq(
-        |              ColumnMetadata(table = Some(value = "t"), name = "c1", typ = SerialType),
-        |              ColumnMetadata(table = Some(value = "t"), name = "c2", typ = IntegerType)
+        |            ArraySeq(
+        |              ColumnMetadata(table = Some("t"), name = "c1", typ = SerialType),
+        |              ColumnMetadata(table = Some("t"), name = "c2", typ = IntegerType)
         |            )
         |          ),
-        |          updater = Some(value = [MemoryDB Updater]),
-        |          deleter = Some(value = [MemoryDB Deleter]),
+        |          updater = Some([MemoryDB Updater]),
+        |          deleter = Some([MemoryDB Deleter]),
         |          mode = Return
         |        )
         |      ),
         |      meta = Metadata(
-        |        columns = ArraySeq(
-        |          ColumnMetadata(table = Some(value = "t"), name = "c1", typ = SerialType),
-        |          ColumnMetadata(table = Some(value = "t"), name = "c2", typ = IntegerType)
+        |        ArraySeq(
+        |          ColumnMetadata(table = Some("t"), name = "c1", typ = SerialType),
+        |          ColumnMetadata(table = Some("t"), name = "c2", typ = IntegerType)
         |        )
         |      )
         |    )
